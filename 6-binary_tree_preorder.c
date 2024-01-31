9f392a02a2a215689534173b4fd6b3e30b124054
julioceaseless <julioceaseless@gmail.com>
2024-01-31 09:29:37 +0000

    function uses preorder traversal to print data from a binary tree

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - reads all data in a binary tree recursively
+ * using preorder traversal.
+ *
+ * @tree: pointer to the root node of the binary tree
+ * @func: pointer to the printing function
+ *
+ * Return: nothing!
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	/* base case */
+	if (tree == NULL)
+		return;
+
+	/* print the data */
+	func(tree->n);
+
+	/* read left nodes */
+	binary_tree_preorder(tree->left, func);
+
+	/* read right nodes */
+	binary_tree_preorder(tree->right, func);
+}
